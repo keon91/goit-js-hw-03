abe3bb83c3adcc63967eb57c6dcb1b522167aca8
[keon91] <maksym.o.vlasov@gmail.com>
2023-11-24 18:04:23 +0200

    test consp

--- js/test.js
+++ js/test.js
@@ -2,6 +2,8 @@
 const start = 6;
 const end = 17;
 let number;
-number = Math.floor(17 / 6);
-  console.log (number)
-
+for (let i = start; i <= end; i++) {
+    if (i / 5 === 0) {
+        console.log(i);
+        break;
+    };
